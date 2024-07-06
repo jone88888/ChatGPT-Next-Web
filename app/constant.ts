1e986357ce31fea38666a7e597e83821aa2f4873
jone88888 <162697705+jone88888@users.noreply.github.com>
2024-07-06 14:57:48 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -1,7 +1,7 @@
-export const OWNER = "Yidadaa";
-export const REPO = "ChatGPT-Next-Web";
+export const OWNER = "";
+export const REPO = "";
 export const REPO_URL = `https://qm.qq.com/q/gYO6lcgd1K`;
-export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
+export const ISSUE_URL = ``;
 export const UPDATE_URL = `${REPO_URL}#keep-updated`;
 export const RELEASE_URL = `${REPO_URL}/releases`;
 export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
